54353f7c2f9ae8e9faacfbe5903c62ce64cc7d33
Giles Alexander <giles.alexander@gmail.com>
2013-08-16 15:17:36 +0100

    Created the server-side app shell.

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject asid "0.0.1-SNAPSHOT"
+  :description "Cool new project to do things and stuff"
+  :dependencies [[org.clojure/clojure "1.5.0"]]
+  :profiles {:dev {:dependencies [[midje "1.5.0"]]}})
+  
---
+++ test/asid/midje.clj
@@ -0,0 +1,5 @@
+(ns asid.midje
+  (:use [midje.sweet]))
+
+(fact (+ 2 2) => 5)
+(fact (+ 2 2) => odd?)
