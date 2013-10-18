418864ed6ce0fab3473372df46d0393fd559221e
Giles Alexander <giles.alexander@gmail.com>
2013-10-18 23:19:21 +0100

    JSON doc processing handles invalid JSON correctly.

--- src/asid/json_doc_exchange.clj
+++ src/asid/json_doc_exchange.clj
@@ -33,33 +33,40 @@
 
 (defn json-documents [handler]
   (fn [req]
-    (let [json-doc (if (content-type-json? req)
+    (let [response (if (content-type-json? req)
                      (try
                        (let [body (rreq/body-string req)]
                          (log/info "JSON body received: " body)
-                         (json/read-str body
-                                        :key-fn keyword))
+                         (let [json-doc (json/read-str body
+                                                       :key-fn keyword)]
+                           (log/debug "Parsed json-doc: " json-doc)
+                           (handler (conj req [:json-doc json-doc]))))
                        (catch Exception ex
                          (log/error ex "JSON could not be parsed.")
                          (-> (rr/response "JSON could not be parsed.")
                              (rr/status 400))))
-                     nil)
-          _ (log/info "Parsed json-doc: " json-doc)
-          response (handler (conj req [:json-doc json-doc]))]
+                     (handler req))]
       (if (content-type-json? response)
         (conj response [:body (json/write-str (:body response))])
         response))))
 
-(fact
+(fact "about valid or no json in the request"
   (let [wrapper (json-documents #(:json-doc %))]
     (wrapper (mr/request :post "/" "body")) => nil?
     (wrapper (-> (mr/request :post "/" "{\"key\":10}")
-                 (mr/header "Content-Type" "app/test+json"))) => {:key 10}
-    (wrapper (-> (mr/request :post "/" "{\"malformed\": 10")
-                 (mr/header "Content-Type" "app/test+json"))) => (contains {:status 400}))
+                 (mr/header "Content-Type" "app/test+json"))) => {:key 10}))
+
+(fact "about invalid json in the request"
   (let [wrapper (json-documents (fn [req] req))]
-    (wrapper (-> (rr/response "hello"))) => (contains {:status 200
-                                                       :body "hello"})
-    (wrapper (-> (rr/response {:key 10})
-                 (rr/content-type "app+json")))) => (contains {:status 200
-                                                               :body "{\"key\":10}"}))
+    (wrapper (-> (mr/request :post "/" "{\"malformed\": 10")
+                 (mr/header "Content-Type" "app/test+json"))) => (contains {:status 400})))
+
+(fact "about json in the response"
+  (let [response-provider (fn [resp]
+                            (let [app (json-documents (fn [_] resp))]
+                              (app (mr/request :get "/anything"))))]
+    (response-provider (-> (rr/response "hello"))) => (contains {:status 200
+                                                                 :body "hello"})
+    (response-provider (-> (rr/response {:key 10})
+                           (rr/content-type "app+json")))) => (contains {:status 200
+                                                                         :body "{\"key\":10}"}))
