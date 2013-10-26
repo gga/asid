2df02af0d74b431149894db5c09992c3360d47e1
Giles Alexander <giles.alexander@gmail.com>
2013-10-26 21:03:06 +0100

    It's actually a response, not a request.

--- test/asid/test/asid_facts.clj
+++ test/asid/test/asid_facts.clj
@@ -15,52 +15,50 @@
   (:status (app (mr/request :get "/"))) => 200)
 
 (fact "POST /identity"
-  (let [req (app (-> (mr/request :post "/identity")
-                     (mr/body "sample-id-seed")))]
-    (:status req) => 201
-    (-> req :headers (get "Location")) => #"\/[a-z0-9-]+$")
-  (let [req (app (-> (mr/request :post "/identity")
-                     (mr/body "")))]
-    (:status req) => 400))
+  (let [resp (app (-> (mr/request :post "/identity")
+                      (mr/body "sample-id-seed")))]
+    (:status resp) => 201
+    (-> resp :headers (get "Location")) => #"\/[a-z0-9-]+$")
+  (let [resp (app (-> (mr/request :post "/identity")
+                      (mr/body "")))]
+    (:status resp) => 400))
 
 (fact "GET /<wallet-id>"
-  (let [req (app (-> (mr/request :get "/bada-bada-bada-1d")
-                     (mr/header "Accept" "application/vnd.org.asidentity.wallet+json")))]
-    (:status req) => 404
-    (:body req) => "Not found.")
+  (let [resp (app (-> (mr/request :get "/bada-bada-bada-1d")
+                      (mr/header "Accept" "application/vnd.org.asidentity.wallet+json")))]
+    (:status resp) => 404
+    (:body resp) => "Not found.")
   (let [wallet (wr/save (w/new-wallet "seed") repo)
-        req (app (-> (mr/request :get (w/uri wallet))
-                     (mr/header "Accept" "text/html, application/xml")))]
-    (:status req) => 200)
+        resp (app (-> (mr/request :get (w/uri wallet))
+                      (mr/header "Accept" "text/html, application/xml")))]
+    (:status resp) => 200)
   (let [wallet (wr/save (w/new-wallet "seed") repo)
-        req (app (-> (mr/request :get (w/uri wallet))
-                     (mr/header "Accept" "application/vnd.org.asidentity.introduction+json")))
-        intro-doc (json/read-str (:body req))]
-    (:status req) => 200
+        resp (app (-> (mr/request :get (w/uri wallet))
+                      (mr/header "Accept" "application/vnd.org.asidentity.introduction+json")))
+        intro-doc (json/read-str (:body resp))]
+    (:status resp) => 200
     (-> intro-doc (get "bag")) => nil?))
 
 (fact "POST /<wallet-id>/bag"
   (let [wallet (wr/save (w/new-wallet "seed") repo)
-        req (app (mr/request :post (w/bag-uri wallet) {"key" "key1" "value" "new-value"}))]
-    (:status req) => 200))
+        resp (app (mr/request :post (w/bag-uri wallet) {"key" "key1" "value" "new-value"}))]
+    (:status resp) => 200))
 
 (fact "POST /<wallet-id>/trustpool"
   (let [wallet (wr/save (w/new-wallet "seed") repo)
-        req (app (-> (mr/request :post (w/trustpool-uri wallet)
-                                 (json/write-str {:name "hello" :challenge ["name"]}))
-                     (mr/header "Content-Type" "application/vnd.org.asidentity.trust-pool+json")))]
-    (:status req) => 201))
+        resp (app (-> (mr/request :post (w/trustpool-uri wallet)
+                                  (json/write-str {:name "hello" :challenge ["name"]}))
+                      (mr/header "Content-Type" "application/vnd.org.asidentity.trust-pool+json")))]
+    (:status resp) => 201))
 
 (fact "GET /<wallet-id>/trustpool/<pool-id>"
   (let [wallet (wr/save (w/new-wallet "seed") repo)
         pool (tpr/save (tp/new-trust-pool "pool" ["name" "dob"]) repo)]
     (ag/trustpool pool wallet)
-    (let [req (app (-> (mr/request :get (tp/uri wallet pool))
-                       (mr/header "Accept" "application/vnd.org.asidentity.trust-pool+json")))]
-      (:status req) => 200)
-    (let [req (app (-> (mr/request :get (str (w/uri wallet) "/trustpool/bada-bada-bada-1d"))
-                       (mr/header "Accept" "application/vnd.org.asidentity.trust-pool+json")))]
-      (:status req) => 404
-      (:body req) => "Not found.")))
-
-
+    (let [resp (app (-> (mr/request :get (tp/uri wallet pool))
+                        (mr/header "Accept" "application/vnd.org.asidentity.trust-pool+json")))]
+      (:status resp) => 200)
+    (let [resp (app (-> (mr/request :get (str (w/uri wallet) "/trustpool/bada-bada-bada-1d"))
+                        (mr/header "Accept" "application/vnd.org.asidentity.trust-pool+json")))]
+      (:status resp) => 404
+      (:body resp) => "Not found.")))
