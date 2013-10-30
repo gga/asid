591770a29169dcf7a22121f5771301d5d57b0939
Giles Alexander <giles.alexander@gmail.com>
2013-10-30 10:14:31 +0000

    Use a dummy mock server as the base.

--- test/asid/test/http_mock.clj
+++ test/asid/test/http_mock.clj
@@ -13,14 +13,13 @@
   {:base-uri base-uri
    :routes (ch/site routes)})
 
-(def ^:dynamic *mocked-server*)
+(def ^:dynamic *mocked-server* (make-server "no-mocked-http-server" []))
 
 (defn- rebuild-clj-response [{headers :headers :as resp}]
   (conj resp [:headers (into {} (for [[hdr val] headers] [(.toLowerCase hdr) val]))]))
 
 (defn- mock-http-request [f request]
-  (if (and (not (nil? *mocked-server*))
-           (.startsWith (:url request) (:base-uri *mocked-server*)))
+  (if (.startsWith (:url request) (:base-uri *mocked-server*))
     (let [fake-handler (:routes *mocked-server*)
           ring-req (mr/request (:method request) (:url request))
           ring-req (conj ring-req [:headers (:headers request)])]
