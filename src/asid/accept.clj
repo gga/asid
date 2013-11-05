9b32f76850cecdab3c285e7e1a84aa1f42ee7737
Giles Alexander <giles.alexander@gmail.com>
2013-11-05 14:39:14 +0000

    Neater, better error message.

--- src/asid/accept.clj
+++ src/asid/accept.clj
@@ -9,18 +9,22 @@
             [clojure.tools.logging :as log]))
 
 (defn- meet-challenge? [conn-req wallet]
-  (let [challenge-set (set (:pool-challenge conn-req))]
+  (let [challenge-set (set (:pool-challenge conn-req))
+        bag-key-set (-> wallet :bag keys set)]
     (log/debug "Challenge: " (cs/join ", " challenge-set))
-    (log/debug "Bag: " (cs/join ", " (-> wallet :bag keys set)))
-    (if (set/subset? challenge-set (-> wallet :bag keys set))
+    (log/debug "Bag keys: " (cs/join ", " bag-key-set))
+    (if (set/subset? challenge-set bag-key-set)
       conn-req
-      (ed/precondition-failed (str "Must have in bag: " (cs/join ", " challenge-set))))))
+      (ed/precondition-failed (str "Missing from bag: "
+                                   (cs/join ", " (set/difference challenge-set
+                                                                 bag-key-set)))))))
 
 (fact "unable to meet the challenge"
   (let [conn-req {:pool-challenge ["dob"]}
         wallet {:bag {"name" "a-name"}}
         challenged (meet-challenge? conn-req wallet)]
-    (:status challenged) => 412))
+    (:status challenged) => 412
+    (:message challenged) => "Missing from bag: dob"))
 
 (defn accept [conn-req wallet updates]
   (fail-> (meet-challenge? conn-req wallet)))
